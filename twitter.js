b1f5bcf84bbbba62847c5532bd2290cb7dd4bec6
Tianyi Ma <tn.ma@outlook.com>
2018-10-20 13:32:01 -0500

    Attempt at getting dataDMUserUpdates component

--- twitter.js
+++ twitter.js
@@ -2,8 +2,14 @@
 var keyPair = HermesCrypto.generateKeyPair();
 var keyPair2 = HermesCrypto.generateKeyPair();
 
+var theirPublicKey = '';
+
 function getTheirPublicKey() {
-  return HermesCrypto.encodeBase64(keyPair2.publicKey);
+  return theirPublicKey;
+}
+
+function getMyPublicKey() {
+  return HermesCrypto.encodeBase64(keyPair.publicKey);
 }
 
 function getMySecretKey() {
@@ -14,6 +20,14 @@ function encryptMessage(message) {
   return HermesCrypto.encryptMessage(message, getTheirPublicKey(), getMySecretKey());
 }
 
+function encryptMessageForSelf(message) {
+  return HermesCrypto.encryptMessage(message, getMyPublicKey(), getMySecretKey());
+}
+
+function lookupTwitterId(id) {
+  return HermesCrypto.encodeBase64(keyPair2.publicKey);
+}
+
 window.addEventListener('message', function(event) {
   if (event.source != window)
     return;
@@ -22,10 +36,17 @@ window.addEventListener('message', function(event) {
     //let port = chrome.runtime.connect();
     if (event.data.type == 'uiDMSendMessage') {
       //console.log(`Request to encrypt: ${event.data.e.text}`);
-      event.data.e.text = encryptMessage(event.data.e.text);
-      window.postMessage({ type: 'uiDMSendMessage_r', e: event.data.e }, '*');
+      if (getTheirPublicKey().length != 0) {
+        var message = event.data.e.text;
+        event.data.e.text = `HERMES_A:${encryptMessage(message)}`;
+        window.postMessage({ type: 'uiDMSendMessage_r', e: event.data.e }, '*');
+        event.data.e.text = `HERMES_B:${encryptMessageForSelf(message)}`;
+        window.postMessage({ type: 'uiDMSendMessage_r', e: event.data.e }, '*');
+      }
     }
     else if (event.data.type == 'uiDMDialogOpenedConversation') {
+      var theirId = event.data.e.recipient.split('-')[1];
+      theirPublicKey = lookupTwitterId(theirId);
       console.log(`New conversation: ${event.data.e.recipient}`);
     }
   }
@@ -41,6 +62,20 @@ function findDMTextbox() {
     for (var j in comps[i].instances) {
       if (comps[i].instances[j].instance.attr.hasOwnProperty('composerEditorSelector') && comps[i].instances[j].instance.attr.composerEditorSelector === '.DMComposer-editor') {
         return comps[i].instances[j];
+      }
+    }
+  }
+}
+
+function findDMConversationComponent() {
+  var comps = DEBUG.registry.components;
+  var i;
+  for (i = 0; i < comps.length; i++) {
+    for (var j in comps[i].instances) {
+      for (var k in comps[i].instances[j].events) {
+        if (comps[i].instances[j].events[k].type.indexOf('dataDMUserUpdates') != -1) {
+          console.trace(comps[i].instances[j]);
+        }
       }
     }
   }
@@ -84,8 +119,14 @@ var listener = function(event) {
 };
 
 
-var interceptListener = (t, e) => {
+var uiDMSendMessageEventInterceptListener = (t, e) => {
   window.postMessage({ type: 'uiDMSendMessage', e: e }, '*');
+  oldT = t;
+};
+
+var dataDMUserUpdatesEventInterceptListener = (t, e) => {
+  console.trace(e);
+  window.postMessage({ type: 'dataDMUserUpdates', e: e }, '*');
   oldT = t;
 };
 
@@ -96,12 +137,25 @@ function uiDMSendMessageEventIntercept() {
       uiDMSendMessageCallback = uiDMSendMessageCallback || sendDMComponent.events[i].callback;
       $(document).off('uiDMSendMessage', undefined, uiDMSendMessageCallback);
 
-      $(document).off('uiDMSendMessage', interceptListener);
-      $(document).on('uiDMSendMessage', interceptListener);
+      $(document).off('uiDMSendMessage', uiDMSendMessageEventInterceptListener);
+      $(document).on('uiDMSendMessage', uiDMSendMessageEventInterceptListener);
 
       window.removeEventListener('message', listener, false);
       window.addEventListener('message', listener, false);
       //console.trace(sendDMComponent);
+    }
+  }
+}
+
+function dataDMUserUpdatesEventIntercept() {
+  var DMConversationComponent = findDMConversationComponent();
+  for (var i = 0; i < DMConversationComponent.events.length; i++) {
+    if (DMConversationComponent.events[i].type.indexOf('dataDMUserUpdates') != -1) {
+      var dataDMUserUpdatesCallback = DMConversationComponent.events[i].callback;
+      $(document).off('dataDMUserUpdates', undefined, dataDMUserUpdatesCallback);
+
+      $(document).off('dataDMUserUpdates', dataDMUserUpdatesEventInterceptListener);
+      $(document).on('dataDMUserUpdates', dataDMUserUpdatesEventInterceptListener);
     }
   }
 }
@@ -115,6 +169,7 @@ var jqueryWaitInterval = setInterval(() => {
       //console.trace(e.recipient);
       window.postMessage({ type: 'uiDMDialogOpenedConversation', e: e }, '*');
       uiDMSendMessageEventIntercept();
+      //dataDMUserUpdatesEventIntercept();
     });
   }
 }, 500);
